28b7cc596fbff30bca5a1388d077ae215f99d4c6
unknown <jeremylscott@gmail.com>
2019-01-13 12:17:17 -0600

    Styled contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './contact.scss'
 import donut from '../../images/sugarboy_donut.png'
+import Header from '../Header/Header'
 
 
 function Contact() {
     return (
         <div>
             <div>
+                <Header/>
                 <div className='pageTitle'>
-                    <h1>Store Locations</h1>
+                    <h1>Store Locations:</h1>
                 </div>
                 <div className='icon'>
                     <div className='donutPic'>
@@ -19,6 +21,8 @@ function Contact() {
                             <p>Prosper East</p>
                             <p>100 Donut Way</p>
                             <p>Prosper, TX 75078</p>
+                            <p>469-555-1212</p>
+                            <p>sugarboy@gmail.com</p>
                         </div>
                         <div className='map'>
                             <iframe className='googleMap' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13346.763684132127!2d-96.7664761!3d33.2483998!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x24644ae7435ef54c!2sSugarboy+Donuts!5e0!3m2!1sen!2sus!4v1547315872916"></iframe>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import './header.css'
+import './header.scss'
 import logo from '../../images/sugarboy_logo.png'
 
 function Header() {
     return (
-        <div>
+        <div className='headerSetup'>
             <img className='headLogo' src={logo} alt='logo'/>
-            <ul className='navBar'>
-                <Link to='/'><ul>Homes</ul></Link>
-                <Link to='/about'><ul>About</ul></Link>
-                <Link to='/contact'><ul>Contact</ul></Link>
-                <Link to='/donuts'><ul>Donuts</ul></Link>
-                <Link to='/kolaches'><ul>Kolaches</ul></Link>
-                <Link to='/drinks'><ul>Drinks</ul></Link>
-            </ul>
+            <nav className='navBar'>
+                <Link className='links' to='/'>Home</Link>
+                <Link className='links' to='/about'>About</Link>
+                <Link className='links' to='/contact'>Contact</Link>
+                <Link className='links' to='/donuts'>Donuts</Link>
+                <Link className='links' to='/kolaches'>Kolaches</Link>
+                <Link className='links2' to='/drinks'>Drinks</Link>
+            </nav>
         </div>
     )
 }
