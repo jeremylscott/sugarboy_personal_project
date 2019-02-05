0a8ded9ac65b607caf0eb9f38e9417bde316f860
unknown <jeremylscott@gmail.com>
2019-02-05 16:41:35 -0600

    Added carousel to home page

--- src/components/HomePic/HomePic.js
+++ src/components/HomePic/HomePic.js
@@ -5,7 +5,12 @@ import './homepic.scss'
 import mainpic9 from '../../images/mainpic9.jpg'
 import mainpic8 from '../../images/mainpic8.jpg'
 import mainpic5 from '../../images/mainpic5.jpg'
+import mainpic3 from '../../images/mainpic3.jpg'
+import mainpic4 from '../../images/mainpic4.jpg'
+import mainpic7 from '../../images/mainpic7.jpg'
 import Home from '../Home/Home'
+import Carousel from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 function HomePic(props) {
 
@@ -23,9 +28,30 @@ function HomePic(props) {
         <div className='homePicCont'>
             <Home/>
             <div className='mainHome'>
-                <img className='mainPic9' src={mainpic9} alt='Donuts'/>
+                <Carousel
+                    animationSpeed={2000}
+                    autoPlay={5000}
+                    stopAutoPlayOnHover
+                    centered
+                    infinite>
+                    <img className='mainPic9' src={mainpic9} alt='donuts'/>
+                    <img className='mainPic9' src={mainpic8} alt='donuts'/>
+                    <img className='mainPic9' src={mainpic5} alt='donuts'/>
+                    <img className='mainPic9' src={mainpic4} alt='donuts'/>
+                    <img className='mainPic9' src={mainpic7} alt='donuts'/>
+                </Carousel>
+
+
+
+
+
+
+
+
+
+                {/* <img className='mainPic9' src={mainpic9} alt='Donuts'/>
                 <img className='mainPic8' src={mainpic8} alt='Donuts'/>
-                <img className='mainPic5' src={mainpic5} alt='Donuts'/>
+                <img className='mainPic5' src={mainpic5} alt='Donuts'/> */}
             </div>
         </div>
     )
