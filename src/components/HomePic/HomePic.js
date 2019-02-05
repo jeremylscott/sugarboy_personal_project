c9e88d02858b613e72a9067774a9e3b46d811a85
unknown <jeremylscott@gmail.com>
2019-02-05 13:40:40 -0600

    Commit prior to implementing slide show on home page

--- src/components/HomePic/HomePic.js
+++ src/components/HomePic/HomePic.js
@@ -2,12 +2,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
 import './homepic.scss'
+import {Slide} from 'react-slideshow-image'
 import mainpic9 from '../../images/mainpic9.jpg'
 import mainpic8 from '../../images/mainpic8.jpg'
 import mainpic5 from '../../images/mainpic5.jpg'
 import Home from '../Home/Home'
 
 function HomePic(props) {
+
+     const slideImages = [
+        '../../images/mainpic9.jpg',
+        '../../images/mainpic8.jpg',
+        '../../images/mainpic5.jpg'
+     ]
     
     if(props.user.isadmin === true) {
         return <Redirect to='/admin'/>   //  Go to admin portal if user is an admin
