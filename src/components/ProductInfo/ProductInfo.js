ac129efbc5d770e3f919291dadeb1ccb0ef25250
unknown <jeremylscott@gmail.com>
2019-02-09 20:50:27 -0600

    Added animated loading screen to the ProductInfo component

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -4,8 +4,26 @@ import {connect} from 'react-redux'
 import {updateProdInfo} from '../../ducks/reducer'
 import './productInfo.scss'
 import Home from '../../components/Home/Home'
+import donut from '../../images/sugarboy_donut.png'
 
 class ProductInfo extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            toggleLoading: true
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                toggleLoading: !this.state.toggleLoading
+            })
+        }, 3000);
+    }
+
+
     
     render() {
         const {prodImg,prodName,prodDesc} = this.props
@@ -19,10 +37,17 @@ class ProductInfo extends Component {
                     <p className='more-info-title'>{prodName}</p>
                     <p className='prodText'>{prodDesc}</p>
                 </div>
-            </div>
-    
-        )
 
+                {this.state.toggleLoading ?
+                    <div className='loadingCont'>
+                        <span className='loading-title'>Loading...</span>
+                        <img src={donut} className='loadingAnimation'/>
+                    </div>
+                :
+                    null
+                }
+            </div>
+        )
     }
 }
 
